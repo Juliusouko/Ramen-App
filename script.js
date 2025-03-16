6aef41752dc3456bae4b20d5353793a9f78b0046
Juliusouko <oukojulius224@gmail.com>
2025-03-16 22:34:20 +0300

    remove carousel feature, had trouble adding images

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const ramens = [
+  { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+  { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+  { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+];
+
+//create a diplay ramen function ()
+function displayRamen()  {
+  //console.log(ramens.length);
+  let slideShow = document.getElementById("slideshow-container");
+  //console.log(slideShow);
+  slideShow.innerHTML = '';
+  ramens.forEach((ramen) => (
+    slideShow.innerHTML 
+    += `${ramen.name}`
+  ));
+}
+displayRamen();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //let slideIndex = 1;
 //showSlides(slideIndex);
 
