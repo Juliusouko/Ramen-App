44e1a39f0dca678a2d9fbceaa563e4505e081523
Julius Ouko <julius@kali.zuku.co.ke>
2025-03-17 13:22:23 -0400

    clean all the files and remove unwanted code and comments

--- javascript/script.js
+++ javascript/script.js
@@ -22,32 +22,28 @@ function displayRamen()  {
         <div class="rating"><p>${ramen.rating}</p></div>
         <div class="comment"><p>${ramen.comment}</p></div>`
   ))
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("image");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slideIndex -1].style.display = "block";
+  }
 }
 displayRamen();
-
-//let slideIndex = 1;
-//showSlides(slideIndex);
-
-//function plusSlides(n) {
-  //showSlides(slideIndex += n);
-//}
-
-//function currentSlide(n) {
-  //showSlides(slideIndex = n);
-//}
-
-//function showSlides(n) {
-  //let i;
-  //let slides = document.getElementsByClassName("mySlides");
-  //let dots = document.getElementsByClassName("dot");
-  //if (n > slides.length) {slideIndex = 1}    
-  //if (n < 1) {slideIndex = slides.length}
-  //for (i = 0; i < slides.length; i++) {
-    //slides[i].style.display = "none";  
-  //}
-  //for (i = 0; i < dots.length; i++) {
-    //dots[i].className = dots[i].className.replace(" active", "");
-  //}
-  //slides[slideIndex-1].style.display = "block";  
-  //dots[slideIndex-1].className += " active";
-//}
